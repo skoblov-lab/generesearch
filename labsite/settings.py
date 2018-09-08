da7ab577f913e5720f6f4cfe2ffcaad46b37ef92
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-09-09 02:07:23 +0300

    bugfix

--- labsite/settings.py
+++ labsite/settings.py
@@ -70,13 +70,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'labsite.context_processors.google_analytics'
             ],
         },
     },
-]
-
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'labsite.context_processors.google_analytics'
 ]
 
 WSGI_APPLICATION = 'labsite.wsgi.application'
